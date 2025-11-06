a7dd62189f8f25207dc4e0eafc5cc19011688e0a
Sebastian Müller <sebastian.m@affinidi.com>
2025-11-06 11:58:03 +0100

    fix: retry configured number of events when sending requests to control plane API

--- packages/meeting_place_control_plane/lib/src/api/retry_interceptor.dart
+++ packages/meeting_place_control_plane/lib/src/api/retry_interceptor.dart
@@ -27,31 +27,26 @@ class RetryInterceptor extends Interceptor {
     DioException err,
     ErrorInterceptorHandler handler,
   ) async {
-    var retryCount = 0;
+    final retryCount = (err.requestOptions.extra['retry_count'] as int?) ?? 0;
 
-    if (_shouldRetry(err)) {
-      while (retryCount < maxRetries) {
-        try {
-          retryCount++;
+    if (_shouldRetry(err) && retryCount < maxRetries) {
+      try {
+        await Future<void>.delayed(retryDelay * (retryCount + 1));
 
-          await Future<void>.delayed(retryDelay * retryCount);
+        final response = await dio.request<dynamic>(
+          err.requestOptions.path,
+          options: Options(
+            method: err.requestOptions.method,
+            headers: err.requestOptions.headers,
+            extra: {...err.requestOptions.extra, 'retry_count': retryCount + 1},
+          ),
+          data: err.requestOptions.data,
+          queryParameters: err.requestOptions.queryParameters,
+        );
 
-          final response = await dio.request<dynamic>(
-            err.requestOptions.path,
-            options: Options(
-              method: err.requestOptions.method,
-              headers: err.requestOptions.headers,
-            ),
-            data: err.requestOptions.data,
-            queryParameters: err.requestOptions.queryParameters,
-          );
-
-          return handler.resolve(response);
-        } on DioException catch (e) {
-          if (retryCount >= maxRetries) {
-            return handler.next(e);
-          }
-        }
+        return handler.resolve(response);
+      } on DioException catch (e) {
+        return handler.next(e);
       }
     }
 

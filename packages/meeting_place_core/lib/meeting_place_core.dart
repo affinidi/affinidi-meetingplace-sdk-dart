6347f623f8e2bb87f4e7c09c8da17c7fe0e71b99
Sebastian Müller <56207234+maindotdev@users.noreply.github.com>
2025-12-09 08:46:35 +0100

    chore: vdip extension - experimental (#34)
    * chore: vdip extension - experimental
    
    * chore: auto register extension

--- packages/meeting_place_core/lib/meeting_place_core.dart
+++ packages/meeting_place_core/lib/meeting_place_core.dart
@@ -32,6 +32,8 @@ export 'src/meeting_place_core_sdk.dart';
 export 'src/meeting_place_core_sdk_options.dart';
 export 'src/meeting_place_core_sdk_error_code.dart';
 export 'src/meeting_place_core_sdk_exception.dart';
+export 'src/extension/vdip/meeting_place_core_sdk_vdip_extension.dart';
+export 'src/extension/vdip/vdip_extension.dart';
 export 'src/protocol/protocol.dart';
 export 'src/protocol/message/plaintext_message_extension.dart';
 export 'src/repository/repository.dart';

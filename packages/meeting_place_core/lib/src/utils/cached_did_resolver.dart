c5bef8450e6ec9d77ef00d863b831c7c4894bad2
Sebastian Rappold <sebastian.m@affinidi.com>
2026-01-19 11:37:01 +0100

    chore: retry on transient network errors in cached DID resolver

--- packages/meeting_place_core/lib/src/utils/cached_did_resolver.dart
+++ packages/meeting_place_core/lib/src/utils/cached_did_resolver.dart
@@ -1,10 +1,23 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:retry/retry.dart';
 import 'package:ssi/ssi.dart';
 
-class CachedDidResolver implements DidResolver {
-  CachedDidResolver({this.resolverAddress});
+import '../../meeting_place_core.dart';
 
-  static Map<String, DidDocument> cacheDIDDocs = {};
+class CachedDidResolver implements DidResolver {
+  CachedDidResolver({
+    this.resolverAddress,
+    required MeetingPlaceCoreSDKLogger logger,
+  }) : _logger = logger;
+
+  static final Map<String, DidDocument> cacheDIDDocs = {};
+  static final int _maxRetryAttempts = 3;
+  static final Duration _maxRetryDelay = const Duration(seconds: 2);
+
   final String? resolverAddress;
+  final MeetingPlaceCoreSDKLogger _logger;
 
   @override
   Future<DidDocument> resolveDid(String did) async {
@@ -12,9 +25,23 @@ class CachedDidResolver implements DidResolver {
       return cacheDIDDocs[did]!;
     }
 
-    final didDocument = await UniversalDIDResolver(
-      resolverAddress: resolverAddress,
-    ).resolveDid(did);
+    final didDocument = await retry(
+      () async => await UniversalDIDResolver(
+        resolverAddress: resolverAddress,
+      ).resolveDid(did),
+      retryIf: (e) =>
+          e is SocketException ||
+          e is TimeoutException ||
+          e is HttpException ||
+          e is HandshakeException ||
+          e is TlsException,
+      onRetry: (e) => _logger.warning(
+        'Retrying unpacking message due to error: $e',
+        name: 'resolveDid',
+      ),
+      maxAttempts: _maxRetryAttempts,
+      maxDelay: _maxRetryDelay,
+    );
 
     cacheDIDDocs[didDocument.id] = didDocument;
     return didDocument;

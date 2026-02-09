58e5af598e7a476946b268cf139a1187dce8a7c1
abrishs <abrish.s@affinidi.com>
2026-02-09 10:41:53 +0100

    chore: add reason field

--- packages/meeting_place_control_plane/lib/src/command/update_offers_score/failed_offer.dart
+++ packages/meeting_place_control_plane/lib/src/command/update_offers_score/failed_offer.dart
@@ -1,8 +1,11 @@
 /// Represents an offer that failed to be updated (e.g. during VRC score
 /// update).
 class FailedOffer {
-  FailedOffer({required this.mnemonic});
+  FailedOffer({required this.mnemonic, this.reason});
 
   /// Mnemonic of the offer that failed.
   final String mnemonic;
+
+  /// Optional reason for the failure.
+  final String? reason;
 }

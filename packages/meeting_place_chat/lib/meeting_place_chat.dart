76d873c0fd28139691bcccc93000070e454be53d
Soroush Yousefpour <soroush.y@affinidi.com>
2026-03-04 09:11:40 +0100

    chore: remove chat presence unnecessary imports (#117)
    * fix: remove unnecessary imports and update chat presence variable naming
    
    * fix: add method to start periodic chat presence updates

--- packages/meeting_place_chat/lib/meeting_place_chat.dart
+++ packages/meeting_place_chat/lib/meeting_place_chat.dart
@@ -9,6 +9,7 @@ export 'src/loggers/meeting_place_chat_sdk_logger.dart';
 export 'src/protocol/protocol.dart';
 export 'src/repository/chat_repository.dart';
 export 'src/service/chat_stream.dart';
+export 'src/sdk/chat_sdk.dart';
 export 'src/sdk/chat_sdk_options.dart';
 export 'src/sdk/group_chat_sdk.dart';
 export 'src/sdk/individual_chat_sdk.dart';

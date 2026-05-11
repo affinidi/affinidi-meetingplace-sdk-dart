3485a7ab180f5f4d5853a759df9f8d4121782c11
Earl Grant Reyes <earl.r@affinidi.com>
2026-05-11 21:06:23 +0800

    fix: extract VdipActivityEventHandler and replace if-chain with switch

--- packages/meeting_place_core/lib/src/event_handler/channel_activity_event_handler.dart
+++ packages/meeting_place_core/lib/src/event_handler/channel_activity_event_handler.dart
@@ -1,4 +1,3 @@
-import 'package:affinidi_tdk_vdip/affinidi_tdk_vdip.dart';
 import 'package:collection/collection.dart';
 import 'package:meeting_place_control_plane/meeting_place_control_plane.dart';
 import 'package:ssi/ssi.dart';
@@ -8,14 +7,13 @@ import '../loggers/meeting_place_core_sdk_logger.dart';
 import '../repository/repository.dart';
 import '../service/channel/channel_service.dart';
 import '../service/connection_manager/connection_manager.dart';
-import '../service/mediator/fetch_messages_options.dart';
 import '../service/mediator/mediator_service.dart';
 import '../vdip/channel_activity_type.dart';
 import '../vdip/vdip_client.dart';
 import 'channel_inauguration_event_handler.dart';
 import 'chat_activity_event_handler.dart';
 import 'control_plane_event_handler_manager_options.dart';
-import 'exceptions/event_handler_exception.dart';
+import 'vdip_activity_event_handler.dart';
 
 class ChannelActivityEventHandler {
   ChannelActivityEventHandler({
@@ -53,47 +51,50 @@ class ChannelActivityEventHandler {
       name: _logKey,
     );
 
-    if (channelActivity.type == 'channel-inauguration') {
-      _logger.info('Processing channel inauguration event', name: _logKey);
-      return ChannelInaugurationEventHandler(
-        wallet: _wallet,
-        mediatorService: _mediatorService,
-        connectionOfferRepository: _connectionOfferRepository,
-        channelService: _channelService,
-        connectionManager: _connectionManager,
-        options: _options,
-        logger: _logger,
-      ).process(channelActivity);
+    switch (channelActivity.type) {
+      case 'channel-inauguration':
+        _logger.info('Processing channel inauguration event', name: _logKey);
+        return ChannelInaugurationEventHandler(
+          wallet: _wallet,
+          mediatorService: _mediatorService,
+          connectionOfferRepository: _connectionOfferRepository,
+          channelService: _channelService,
+          connectionManager: _connectionManager,
+          options: _options,
+          logger: _logger,
+        ).process(channelActivity);
+      case 'chat-activity':
+        _logger.info('Processing chat activity event', name: _logKey);
+        return ChatActivityEventHandler(
+          wallet: _wallet,
+          connectionManager: _connectionManager,
+          connectionOfferRepository: _connectionOfferRepository,
+          channelService: _channelService,
+          mediatorService: _mediatorService,
+          options: _options,
+          logger: _logger,
+        ).process(channelActivity);
+      case ChannelActivityType.vdipRequestIssuance ||
+          ChannelActivityType.vdipIssuedCredentials:
+        _logger.info(
+          'Processing VDIP activity event: ${channelActivity.type}',
+          name: _logKey,
+        );
+        return VdipActivityEventHandler(
+          wallet: _wallet,
+          mediatorService: _mediatorService,
+          channelService: _channelService,
+          connectionManager: _connectionManager,
+          logger: _logger,
+          vdipClient: _vdipClient,
+        ).process(channelActivity);
+      default:
+        _logger.warning(
+          'Unsupported channel activity type: ${channelActivity.type}',
+          name: _logKey,
+        );
+        return [];
     }
-
-    if (channelActivity.type == 'chat-activity') {
-      _logger.info('Processing chat activity event', name: _logKey);
-      return ChatActivityEventHandler(
-        wallet: _wallet,
-        connectionManager: _connectionManager,
-        connectionOfferRepository: _connectionOfferRepository,
-        channelService: _channelService,
-        mediatorService: _mediatorService,
-        options: _options,
-        logger: _logger,
-      ).process(channelActivity);
-    }
-
-    if (channelActivity.type == ChannelActivityType.vdipRequestIssuance ||
-        channelActivity.type == ChannelActivityType.vdipIssuedCredentials) {
-      _logger.info(
-        'Processing VDIP activity event: ${channelActivity.type}',
-        name: _logKey,
-      );
-      return _processVdipActivity(channelActivity);
-    }
-
-    _logger.warning(
-      'Unsupported channel activity type: ${channelActivity.type}',
-      name: _logKey,
-    );
-
-    return [];
   }
 
   bool hasChannelActivityBeenProcessed(
@@ -106,53 +107,5 @@ class ChannelActivityEventHandler {
               event.data.type == channelActivity.type,
         ) !=
         null;
-  }
-
-  Future<List<Channel>> _processVdipActivity(
-    ChannelActivity channelActivity,
-  ) async {
-    final channel = await _channelService.findChannelByDid(channelActivity.did);
-    final permanentChannelDid = channel.permanentChannelDid;
-    if (permanentChannelDid == null) {
-      throw EventHandlerException.missingPermanentChannelDid(
-        channelId: channel.id,
-      );
-    }
-    final didManager = await _connectionManager.getDidManagerForDid(
-      _wallet,
-      permanentChannelDid,
-    );
-
-    final messages = await _mediatorService.fetchMessages(
-      didManager: didManager,
-      mediatorDid: channel.mediatorDid,
-      options: FetchMessagesOptions(
-        deleteOnRetrieve: false,
-        filterByMessageTypes: [
-          VdipRequestIssuanceMessage.messageType.toString(),
-          VdipIssuedCredentialMessage.messageType.toString(),
-        ],
-      ),
-    );
-
-    for (final message in messages) {
-      _vdipClient.dispatch(message.plainTextMessage);
-      final messageHash = message.messageHash;
-      if (messageHash != null) {
-        await _mediatorService.deleteMessages(
-          didManager: didManager,
-          mediatorDid: channel.mediatorDid,
-          messageHashes: [messageHash],
-        );
-      } else {
-        _logger.warning(
-          'Skipping VDIP mediator message deletion because message hash is '
-          'missing',
-          name: _logKey,
-        );
-      }
-    }
-
-    return [];
   }
 }
--- packages/meeting_place_core/test/unit/event_handler/channel_activity_event_handler_test.dart
+++ packages/meeting_place_core/test/unit/event_handler/channel_activity_event_handler_test.dart
@@ -1,15 +1,6 @@
-import 'package:affinidi_tdk_vdip/affinidi_tdk_vdip.dart';
-import 'package:didcomm/didcomm.dart';
-import 'package:meeting_place_control_plane/meeting_place_control_plane.dart'
-    hide ContactCard;
-import 'package:meeting_place_core/src/entity/channel.dart';
+import 'package:meeting_place_control_plane/meeting_place_control_plane.dart';
 import 'package:meeting_place_core/src/event_handler/channel_activity_event_handler.dart';
 import 'package:meeting_place_core/src/event_handler/control_plane_event_handler_manager_options.dart';
-import 'package:meeting_place_core/src/event_handler/exceptions/event_handler_exception.dart';
-import 'package:meeting_place_core/src/meeting_place_core_sdk_error_code.dart';
-import 'package:meeting_place_core/src/protocol/contact_card/contact_card.dart';
-import 'package:meeting_place_core/src/service/mediator/fetch_messages_options.dart';
-import 'package:meeting_place_core/src/service/mediator/mediator_message.dart';
 import 'package:meeting_place_core/src/vdip/channel_activity_type.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -20,105 +11,40 @@ import 'mocks/mocks.dart';
 void main() {
   late ChannelActivityEventHandler handler;
   late MockLogger mockLogger;
-  late MockWallet mockWallet;
-  late MockMediatorService mockMediatorService;
   late MockConnectionOfferRepository mockConnectionOfferRepository;
-  late MockChannelService mockChannelService;
-  late MockConnectionManager mockConnectionManager;
-  late MockDidManager mockDidManager;
   late MockVdipClient mockVdipClient;
-
-  const permanentChannelDid = 'did:key:permanent-channel';
-  const channelDid = 'did:key:channel';
-  const mediatorDid = 'did:web:mediator';
-  const messageHash = 'hash-123';
-
-  final channel = Channel(
-    offerLink: 'offer-link',
-    publishOfferDid: channelDid,
-    mediatorDid: mediatorDid,
-    status: ChannelStatus.inaugurated,
-    isConnectionInitiator: true,
-    contactCard: ContactCard(
-      did: 'did:key:other-party',
-      type: 'individual',
-      contactInfo: const {'fullName': 'Alice'},
-    ),
-    type: ChannelType.individual,
-    permanentChannelDid: permanentChannelDid,
-  );
 
   final event = ChannelActivity(
     id: const Uuid().v4(),
-    did: channelDid,
+    did: 'did:key:channel',
     type: ChannelActivityType.vdipRequestIssuance,
   );
 
   final otherVdipEvent = ChannelActivity(
     id: const Uuid().v4(),
-    did: channelDid,
+    did: 'did:key:channel',
     type: ChannelActivityType.vdipIssuedCredentials,
   );
 
-  setUpAll(() {
-    registerFallbackValue(const FetchMessagesOptions());
-    registerFallbackValue(
-      PlainTextMessage(
-        id: const Uuid().v4(),
-        type: Uri.parse('https://example.com/fallback'),
-        body: {},
-      ),
-    );
-  });
-
   setUp(() {
     mockLogger = MockLogger();
-    mockWallet = MockWallet();
-    mockMediatorService = MockMediatorService();
     mockConnectionOfferRepository = MockConnectionOfferRepository();
-    mockChannelService = MockChannelService();
-    mockConnectionManager = MockConnectionManager();
-    mockDidManager = MockDidManager(did: permanentChannelDid);
     mockVdipClient = MockVdipClient();
 
     when(
       () => mockLogger.info(any(), name: any(named: 'name')),
     ).thenReturn(null);
-    when(
-      () => mockLogger.warning(any(), name: any(named: 'name')),
-    ).thenReturn(null);
 
     handler = ChannelActivityEventHandler(
-      wallet: mockWallet,
-      mediatorService: mockMediatorService,
-      connectionManager: mockConnectionManager,
-      channelService: mockChannelService,
+      wallet: MockWallet(),
+      mediatorService: MockMediatorService(),
+      connectionManager: MockConnectionManager(),
+      channelService: MockChannelService(),
       connectionOfferRepository: mockConnectionOfferRepository,
       options: const ControlPlaneEventHandlerManagerOptions(),
       logger: mockLogger,
       vdipClient: mockVdipClient,
     );
-
-    when(
-      () => mockChannelService.findChannelByDid(channelDid),
-    ).thenAnswer((_) async => channel);
-
-    when(
-      () => mockConnectionManager.getDidManagerForDid(
-        mockWallet,
-        permanentChannelDid,
-      ),
-    ).thenAnswer((_) async => mockDidManager);
-
-    when(
-      () => mockMediatorService.deleteMessages(
-        didManager: mockDidManager,
-        mediatorDid: mediatorDid,
-        messageHashes: [messageHash],
-      ),
-    ).thenAnswer((_) async {});
-
-    when(() => mockVdipClient.dispatch(any())).thenReturn(null);
   });
 
   group('ChannelActivityEventHandler', () {
@@ -144,130 +70,5 @@ void main() {
         isFalse,
       );
     });
-
-    test('fetches, dispatches, and deletes VDIP messages', () async {
-      final mediatorMessage = MediatorMessage(
-        plainTextMessage: PlainTextMessage(
-          id: const Uuid().v4(),
-          type: VdipRequestIssuanceMessage.messageType,
-          body: {},
-        ),
-        messageHash: messageHash,
-      );
-
-      when(
-        () => mockMediatorService.fetchMessages(
-          didManager: mockDidManager,
-          mediatorDid: mediatorDid,
-          options: any(named: 'options'),
-        ),
-      ).thenAnswer((_) async => [mediatorMessage]);
-
-      final result = await handler.process(event);
-
-      expect(result, isEmpty);
-
-      final verification = verify(
-        () => mockMediatorService.fetchMessages(
-          didManager: mockDidManager,
-          mediatorDid: mediatorDid,
-          options: captureAny(named: 'options'),
-        ),
-      )..called(1);
-
-      final options = verification.captured.single as FetchMessagesOptions;
-      expect(
-        options.filterByMessageTypes,
-        equals([
-          VdipRequestIssuanceMessage.messageType.toString(),
-          VdipIssuedCredentialMessage.messageType.toString(),
-        ]),
-      );
-      expect(options.deleteOnRetrieve, isFalse);
-
-      verify(
-        () => mockVdipClient.dispatch(mediatorMessage.plainTextMessage),
-      ).called(1);
-      verify(
-        () => mockMediatorService.deleteMessages(
-          didManager: mockDidManager,
-          mediatorDid: mediatorDid,
-          messageHashes: [messageHash],
-        ),
-      ).called(1);
-    });
-
-    test('throws domain exception when channel lacks permanent DID', () async {
-      final channelWithoutPermanentDid = Channel(
-        offerLink: 'offer-link',
-        publishOfferDid: channelDid,
-        mediatorDid: mediatorDid,
-        status: ChannelStatus.inaugurated,
-        isConnectionInitiator: true,
-        contactCard: ContactCard(
-          did: 'did:key:other-party',
-          type: 'individual',
-          contactInfo: const {'fullName': 'Alice'},
-        ),
-        type: ChannelType.individual,
-      );
-
-      when(
-        () => mockChannelService.findChannelByDid(channelDid),
-      ).thenAnswer((_) async => channelWithoutPermanentDid);
-
-      expect(
-        handler.process(event),
-        throwsA(
-          isA<EventHandlerException>().having(
-            (error) => error.code,
-            'code',
-            MeetingPlaceCoreSDKErrorCode.channelMissingPermanentChannelDid,
-          ),
-        ),
-      );
-    });
-
-    test(
-      'logs warning and skips deletion when message hash is missing',
-      () async {
-        final mediatorMessage = MediatorMessage(
-          plainTextMessage: PlainTextMessage(
-            id: const Uuid().v4(),
-            type: VdipRequestIssuanceMessage.messageType,
-            body: {},
-          ),
-        );
-
-        when(
-          () => mockMediatorService.fetchMessages(
-            didManager: mockDidManager,
-            mediatorDid: mediatorDid,
-            options: any(named: 'options'),
-          ),
-        ).thenAnswer((_) async => [mediatorMessage]);
-
-        final result = await handler.process(event);
-
-        expect(result, isEmpty);
-        verify(
-          () => mockVdipClient.dispatch(mediatorMessage.plainTextMessage),
-        ).called(1);
-        verifyNever(
-          () => mockMediatorService.deleteMessages(
-            didManager: mockDidManager,
-            mediatorDid: mediatorDid,
-            messageHashes: [messageHash],
-          ),
-        );
-        verify(
-          () => mockLogger.warning(
-            'Skipping VDIP mediator message deletion because message hash is '
-            'missing',
-            name: 'ChannelActivityEventHandler',
-          ),
-        ).called(1);
-      },
-    );
   });
 }

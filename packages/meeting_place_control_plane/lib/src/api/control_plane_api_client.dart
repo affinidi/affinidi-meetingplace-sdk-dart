849c6cce8a03b16e1ecda3672ecf63cc6f04ac82
Soroush Yousefpour <soroush.y@affinidi.com>
2026-05-18 14:24:46 +0200

    fix: reorder import statements for idle timeout configurator

--- packages/meeting_place_control_plane/lib/src/api/control_plane_api_client.dart
+++ packages/meeting_place_control_plane/lib/src/api/control_plane_api_client.dart
@@ -6,10 +6,10 @@ import '../constants/sdk_constants.dart';
 import '../control_plane_sdk.dart';
 import '../loggers/control_plane_sdk_logger.dart';
 import '../loggers/default_control_plane_sdk_logger.dart';
-import 'idle_timeout_configurator_stub.dart'
-    if (dart.library.io) 'idle_timeout_configurator_io.dart';
 import 'api_client.dart' as api_client;
 import 'control_plane_api_client_options.dart';
+import 'idle_timeout_configurator_stub.dart'
+    if (dart.library.io) 'idle_timeout_configurator_io.dart';
 import 'refresh_auth_credentials_interceptor.dart';
 import 'retry_interceptor.dart';
 

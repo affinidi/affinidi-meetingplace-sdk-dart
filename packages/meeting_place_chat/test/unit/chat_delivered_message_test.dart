97bc545c588a878a6da5c376b33227b386a904c8
Soroush Yousefpour <soroush.y@affinidi.com>
2026-05-19 15:44:33 +0200

    fix: decouple unit test from sqflite_ffi dependency
    Use inline UnsupportedMatrixDatabaseFactory in chat_delivered_message_test
    instead of importing getMatrixConfig() which pulls in sqflite_common_ffi.

--- packages/meeting_place_chat/test/unit/chat_delivered_message_test.dart
+++ packages/meeting_place_chat/test/unit/chat_delivered_message_test.dart
@@ -9,7 +9,6 @@ import '../utils/repository/chat_repository_impl.dart';
 import '../utils/repository/connection_group_offer_repository_impl.dart';
 import '../utils/repository/connection_offer_repository_impl.dart';
 import '../utils/repository/key_repository_impl.dart';
-import '../utils/sdk.dart';
 import '../utils/storage/in_memory_storage.dart';
 
 // Captures calls to sendPlainTextMessage for assertion.
@@ -73,7 +72,10 @@ void main() {
       ),
       mediatorDid: mediatorDid,
       controlPlaneDid: 'did:peer:control-plane',
-      matrixConfig: getMatrixConfig(),
+      matrixConfig: MatrixConfig(
+        homeserver: Uri.parse('https://matrix.example.com'),
+        databaseFactory: const UnsupportedMatrixDatabaseFactory(),
+      ),
     );
 
     sdk = _SpyChatSDK(

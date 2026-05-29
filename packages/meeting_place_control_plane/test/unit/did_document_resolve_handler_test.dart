7f9afafb8079f0a46f004fcafbec3a1c3b80cb59
Soroush Yousefpour <soroush.y@affinidi.com>
2026-05-29 14:15:07 +0200

    test: add unit tests for ResolveDidDocumentHandler
    - success path, encoded-port did:web host, invalid DID, empty string
    - resolver error wraps to generic exception with innerException preserved

---
+++ packages/meeting_place_control_plane/test/unit/did_document_resolve_handler_test.dart
@@ -0,0 +1,112 @@
+import 'package:meeting_place_control_plane/src/command/did_document_resolve/did_document_resolve.dart';
+import 'package:meeting_place_control_plane/src/command/did_document_resolve/did_document_resolve_exception.dart';
+import 'package:meeting_place_control_plane/src/command/did_document_resolve/did_document_resolve_handler.dart';
+import 'package:meeting_place_control_plane/src/control_plane_sdk_error_code.dart';
+import 'package:meeting_place_control_plane/src/loggers/control_plane_sdk_logger.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:ssi/ssi.dart';
+import 'package:test/test.dart';
+
+class MockDidResolver extends Mock implements DidResolver {}
+
+class MockControlPlaneSDKLogger extends Mock implements ControlPlaneSDKLogger {}
+
+DidDocument _didDocument(String did) => DidDocument.fromJson({
+  '@context': ['https://www.w3.org/ns/did/v1'],
+  'id': did,
+  'verificationMethod': const <Object>[],
+  'authentication': const <Object>[],
+});
+
+void main() {
+  late MockDidResolver mockDidResolver;
+  late MockControlPlaneSDKLogger mockLogger;
+  late ResolveDidDocumentHandler handler;
+
+  setUp(() {
+    mockDidResolver = MockDidResolver();
+    mockLogger = MockControlPlaneSDKLogger();
+    handler = ResolveDidDocumentHandler(
+      didResolver: mockDidResolver,
+      logger: mockLogger,
+    );
+  });
+
+  group('ResolveDidDocumentHandler', () {
+    test('returns resolved DID document on success', () async {
+      const did = 'did:web:example.com:user:alice';
+      final document = _didDocument(did);
+
+      when(
+        () => mockDidResolver.resolveDid(did),
+      ).thenAnswer((_) async => document);
+
+      final output = await handler.handle(ResolveDidDocumentCommand(did: did));
+
+      expect(output.didDocument.id, equals(did));
+    });
+
+    test('accepts did:web hosts with encoded ports', () async {
+      const did = 'did:web:example.com%3A8080:user:alice';
+      final document = _didDocument(did);
+
+      when(
+        () => mockDidResolver.resolveDid(did),
+      ).thenAnswer((_) async => document);
+
+      final output = await handler.handle(ResolveDidDocumentCommand(did: did));
+
+      expect(output.didDocument.id, equals(did));
+    });
+
+    test('throws invalidDid for non-did:web input', () {
+      const did = 'did:key:z6Mkf5rGMoatrSj1f4CyvuHBeXJELe9y84QFF7oNQLFMkRx';
+
+      expect(
+        () => handler.handle(ResolveDidDocumentCommand(did: did)),
+        throwsA(
+          isA<ResolveDidDocumentException>().having(
+            (e) => e.code,
+            'code',
+            ControlPlaneSDKErrorCode.resolveDidDocumentInvalidDid,
+          ),
+        ),
+      );
+      verifyNever(() => mockDidResolver.resolveDid(any()));
+    });
+
+    test('throws invalidDid for empty string', () {
+      expect(
+        () => handler.handle(ResolveDidDocumentCommand(did: '')),
+        throwsA(
+          isA<ResolveDidDocumentException>().having(
+            (e) => e.code,
+            'code',
+            ControlPlaneSDKErrorCode.resolveDidDocumentInvalidDid,
+          ),
+        ),
+      );
+      verifyNever(() => mockDidResolver.resolveDid(any()));
+    });
+
+    test('throws generic when resolver throws', () async {
+      const did = 'did:web:example.com:user:alice';
+      final resolverError = Exception('network error');
+
+      when(() => mockDidResolver.resolveDid(did)).thenThrow(resolverError);
+
+      expect(
+        () => handler.handle(ResolveDidDocumentCommand(did: did)),
+        throwsA(
+          isA<ResolveDidDocumentException>()
+              .having(
+                (e) => e.code,
+                'code',
+                ControlPlaneSDKErrorCode.resolveDidDocumentGeneric,
+              )
+              .having((e) => e.innerException, 'innerException', resolverError),
+        ),
+      );
+    });
+  });
+}

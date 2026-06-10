6245aa471f02fdccd3b2092ae94c0fa564777d4a
Sebastian Rappold <sebastian.m@affinidi.com>
2026-06-10 10:26:56 +0200

    fix: use enum for chat event types

--- packages/meeting_place_chat/lib/src/transport/matrix/outgoing/profile_hash_room_event.dart
+++ packages/meeting_place_chat/lib/src/transport/matrix/outgoing/profile_hash_room_event.dart
@@ -1,9 +1,11 @@
 import 'package:meeting_place_core/meeting_place_core.dart';
 
+import '../matrix_chat_event_type.dart';
+
 class ProfileHashRoomEvent extends MatrixOutgoingMessage {
   ProfileHashRoomEvent({required super.senderDid, required String profileHash})
     : super(
-        type: 'com.affinidi.chat.profile-hash',
+        type: MatrixChatEventType.profileHash,
         content: {'profile_hash': profileHash},
       );
 }

6b06ba49b7f23a3634079c21786423cd77deaa76
Sebastian Rappold <sebastian.m@affinidi.com>
2026-06-11 14:23:48 +0200

    test: fix test cases

--- packages/meeting_place_chat/lib/src/event/chat_event_conversion.dart
+++ packages/meeting_place_chat/lib/src/event/chat_event_conversion.dart
@@ -8,8 +8,17 @@ import 'incoming_chat_event.dart';
 
 extension MatrixRoomEventToChatEvent on MatrixRoomEvent {
   ChatEvent toChatEvent() {
-    return switch (type) {
-      MatrixChatEventType.chatEffect => ChatEffectEvent(
+    // First, try to match against Matrix-specific event types
+    if (type == MatrixChatEventType.chatEffect) {
+      return ChatEffectEvent(
+        effectName: content['effect'] as String? ?? '',
+      );
+    }
+
+    // Then, try to match against DIDComm protocol types
+    final chatProtocol = protocol.ChatProtocol.byValue(type);
+    return switch (chatProtocol) {
+      protocol.ChatProtocol.chatEffect => ChatEffectEvent(
         effectName: content['effect'] as String? ?? '',
       ),
       _ => const ChatMessageEvent(),

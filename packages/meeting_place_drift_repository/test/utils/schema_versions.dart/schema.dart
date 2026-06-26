3e5f7e104f3a8693efe1b00dd01cf783cf541cd7
Earl Grant Reyes <earl.r@affinidi.com>
2026-06-26 20:36:47 +0800

    fix: consolidate call exports and drop stray branch leaks

--- packages/meeting_place_drift_repository/test/utils/schema_versions.dart/schema.dart
+++ packages/meeting_place_drift_repository/test/utils/schema_versions.dart/schema.dart
@@ -23,5 +23,5 @@ class GeneratedHelper implements SchemaInstantiationHelper {
     }
   }
 
-  static const versions = const [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  static const versions = const [1, 2, 3];
 }

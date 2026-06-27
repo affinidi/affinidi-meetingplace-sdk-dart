f1d95473bcd8ef03acdce0037f583d43f4a206db
Earl Grant Reyes <earl.r@affinidi.com>
2026-06-27 12:27:16 +0800

    chore: temporarily add hostedMedia as current app state is looking for it

--- packages/meeting_place_core/lib/src/protocol/attachment/attachment_format.dart
+++ packages/meeting_place_core/lib/src/protocol/attachment/attachment_format.dart
@@ -1,6 +1,9 @@
 enum AttachmentFormat {
   contactCard('https://affinidi.io/mpx/core-sdk/attachment/contact-card'),
   imageSelfie('https://affinidi.io/mpx/core-sdk/attachment/image-selfie'),
+  // TODO (earl): remove this once the app is updated. currently app needs this
+  // especially to pass melos analyze in precommit hook.
+  hostedMedia('https://affinidi.io/mpx/core-sdk/attachment/hosted-media'),
   verifiablePresentation(
     'https://affinidi.io/mpx/core-sdk/attachment/verifiable-presentation',
   );

71a85890354a2f49c223538d9e576cd05cb33ca1
Earl Grant Reyes <earl.r@affinidi.com>
2026-07-13 18:52:46 +0800

    chore: added example main.dart

---
+++ packages/meeting_place_matrix/example/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Please refer to the other folders within this example project for more
+  // complete examples of how to use the MeetingPlaceMatrix SDK.
+  //
+  // Examples:
+  // - /calls: Demonstrates how to make and receive calls using the SDK.
+  // - /chat_media: Demonstrates how to send and receive text messages with media attachments.
+  // - /group: Demonstrates how to create and manage group chats using the SDK.
+  // - /media: Demonstrates how to send and receive media messages using the SDK.
+}

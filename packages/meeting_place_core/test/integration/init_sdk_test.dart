afdc8f6c20d6a5f0272738340996af4d21512229
Marco Attanasio <marco.a@affinidi.com>
2026-08-02 19:12:27 +0200

    chore: update integration tests setup

--- packages/meeting_place_core/test/integration/init_sdk_test.dart
+++ packages/meeting_place_core/test/integration/init_sdk_test.dart
@@ -2,6 +2,7 @@
 library;
 
 import 'package:meeting_place_core/meeting_place_core.dart';
+import 'package:meeting_place_core/src/service/connection_offer/connection_offer_exception.dart';
 import 'package:ssi/ssi.dart';
 import 'package:test/test.dart';
 import 'package:uuid/uuid.dart';
@@ -90,7 +91,13 @@ void main() async {
         ),
         type: SDKConnectionOfferType.groupInvitation,
       ),
-      throwsA(isA<UnimplementedError>()),
+      throwsA(
+        isA<ConnectionOfferException>().having(
+          (error) => error.innerException,
+          'innerException',
+          isA<UnimplementedError>(),
+        ),
+      ),
     );
   });
 

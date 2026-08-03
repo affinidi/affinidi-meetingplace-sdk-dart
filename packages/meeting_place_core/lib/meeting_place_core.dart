7a49255e1b519993f5b02514d4c0d11b2c3cb9f8
marco-a-affinidi <marco.a@affinidi.com>
2026-08-03 13:46:28 +0200

    chore: expose ChannelServiceException (#306)

--- packages/meeting_place_core/lib/meeting_place_core.dart
+++ packages/meeting_place_core/lib/meeting_place_core.dart
@@ -40,6 +40,7 @@ export 'src/protocol/protocol.dart';
 export 'src/repository/repository.dart';
 export 'src/sdk/results/register_for_didcomm_notifications_result.dart';
 export 'src/sdk/sdk.dart';
+export 'src/service/channel/channel_service_exception.dart';
 export 'src/service/config.dart';
 export 'src/service/core_sdk_stream_subscription.dart';
 export 'src/service/mediator/mediator_message.dart';
